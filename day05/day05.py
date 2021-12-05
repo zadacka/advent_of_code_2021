41938d7af7d75fb126af85682b577162e145e95d
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2021-12-05 23:00:28 +0000

    Yuk yuk yuk yuk.
    Could use a refactor and maybe some nicer separation into helper methods.
    
    But it works.

--- day05/day05.py
+++ day05/day05.py
@@ -1,30 +1,5 @@
 import os.path
-
-
-class Vent:
-
-    # def determine_type(self):
-    #     if self.start_x == self.end_x and self.start_y == self.end_y:
-    #         return "point"
-    #     if self.start_x == self.end_x:
-    #         return "horixontal"
-    #     elif self.start_y == self.end_y:
-    #         return "vertical"
-    #     elif (self.end_x - self.start_x) == (self.end_y - self.start_y):
-    #         return "diagonal"
-    #     else:
-    #         raise ValueError(
-    #             "Unknown line type {},{} -> {}{}".format(self.start_x, self.start_y, self.end_x, self.end_y))
-    #
-    # def __init__(self, start_x, start_y, end_x, end_y) -> None:
-    #     super().__init__()
-    #     # ensure start < end so future line processing can assume that order
-    #     self.start_x = min(int(start_x), int(end_x))
-    #     self.start_y = min(int(start_y), int(end_y))
-    #     self.end_x = max(int(start_x), int(end_x))
-    #     self.end_y = max(int(start_y), int(end_y))
-    #
-    #     self.type = self.determine_type()
+from itertools import zip_longest, chain
 
 
 def parse_vent_input_file(filename):
@@ -33,6 +8,68 @@ def parse_vent_input_file(filename):
     with open(file_path) as f:
         for line in f.readlines():
             start, arrow, end = line.split()
-            vent = Vent(*start.split(','), *end.split(','))
-            result.append(vent)
+            x1, y1 = start.split(',')
+            x2, y2 = end.split(',')
+            result.append({"x1": int(x1), "y1": int(y1), "x2": int(x2), "y2": int(y2)})
     return result
+
+
+def generate_blank_grid(lines):
+    columns = max(*(start["x1"] for start in lines), *(start["x2"] for start in lines)) + 1
+    rows = max(*(start["y1"] for start in lines), *(start["y2"] for start in lines)) + 1
+    return [[0] * columns for _ in range(rows)]
+
+
+def print_map(test_grid):
+    map = ""
+    for line in test_grid:
+        map += ''.join([str(c) if c != 0 else '.' for c in line])
+        map += '\n'
+    return map
+
+
+def line_to_points(line):
+    x_step = 1 if line["x2"] >= line["x1"] else -1
+    y_step = 1 if line["y2"] >= line["y1"] else -1
+    x_values = list(range(line["x1"], line["x2"] + x_step, x_step))
+    y_values = list(range(line["y1"], line["y2"] + y_step, y_step))
+
+    fill_value = line["x1"] if len(x_values) == 1 else line["y1"]  # a line of one value = not changing
+    return [(x, y) for x, y in zip_longest(x_values, y_values, fillvalue=fill_value)]
+
+
+def is_diagonal(line):
+    if (line["x1"] != line["x2"]) and (line["y1"] != line["y2"]):
+        return True
+    return False
+
+
+def add_line_to_grid(line, grid):
+    for point in line_to_points(line):
+        x, y = point
+        grid[y][x] += 1
+
+
+def generate_grid(lines, skip_diagonals=True):
+    grid = generate_blank_grid(lines)
+    for line in lines:
+        if skip_diagonals and is_diagonal(line):
+            pass
+        else:
+            add_line_to_grid(line, grid)
+    return grid
+
+
+def calculate_overlap_count(grid):
+    return sum(x >= 2 for x in chain(*grid))
+
+
+if __name__ == "__main__":
+    input = parse_vent_input_file("day05_real_data.txt")
+    grid = generate_grid(input)
+    overlap = calculate_overlap_count(grid)
+    print("The grid has {} points where two or more vent lines overlap".format(overlap))
+
+    grid_with_diagonals = generate_grid(input, skip_diagonals=False)
+    overlap2 = calculate_overlap_count(grid_with_diagonals)
+    print("The grid has {} points where two or more vent lines overlap when we include diagonals".format(overlap2))
--- day05/day05_test.py
+++ day05/day05_test.py
@@ -1,34 +1,107 @@
+from textwrap import dedent
+
 from testfixtures import compare
 
-from day05.day05 import parse_vent_input_file, Vent
+from day05.day05 import parse_vent_input_file, line_to_points, print_map, generate_grid, calculate_overlap_count
+
+test_lines = [
+    {"x1": 0, "y1": 9, "x2": 5, "y2": 9},
+    {"x1": 8, "y1": 0, "x2": 0, "y2": 8},
+    {"x1": 9, "y1": 4, "x2": 3, "y2": 4},
+    {"x1": 2, "y1": 2, "x2": 2, "y2": 1},
+    {"x1": 7, "y1": 0, "x2": 7, "y2": 4},
+    {"x1": 6, "y1": 4, "x2": 2, "y2": 0},
+    {"x1": 0, "y1": 9, "x2": 2, "y2": 9},
+    {"x1": 3, "y1": 4, "x2": 1, "y2": 4},
+    {"x1": 0, "y1": 0, "x2": 8, "y2": 8},
+    {"x1": 5, "y1": 5, "x2": 8, "y2": 2},
+]
+
+test_grid = [
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 1, 1, 2, 1, 1, 1, 2, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [2, 2, 2, 1, 1, 1, 0, 0, 0, 0],
+]
 
 
 def test_parse_vent_input_file():
-    lines = parse_vent_input_file("day05_test_data.txt")
-    expected = [
-        ((0, 9), (5, 9))
-        ((8, 0), (0, 8))
-        ((9, 4), (3, 4))
-        ((2, 2), (2, 1))
-        ((7, 0), (7, 4))
-        ((6, 4), (2, 0))
-        ((0, 9), (2, 9))
-        ((3, 4), (1, 4))
-        ((0, 0), (8, 8))
-        ((5, 5), (8, 2))
-        # Vent(0, 9, 5, 9),
-        # Vent(8, 0, 0, 8),
-        # Vent(9, 4, 3, 4),
-        # Vent(2, 2, 2, 1),
-        # Vent(7, 0, 7, 4),
-        # Vent(6, 4, 2, 0),
-        # Vent(0, 9, 2, 9),
-        # Vent(3, 4, 1, 4),
-        # Vent(0, 0, 8, 8),
-        # Vent(5, 5, 8, 2),
+    actual = parse_vent_input_file("day05_test_data.txt")
+    compare(test_lines, actual=actual)
+
+
+def test_line_to_points():
+    point = line_to_points({"x1": 0, "y1": 0, "x2": 0, "y2": 0})
+    compare(point, expected=[(0, 0)])
+
+    vertical = line_to_points({"x1": 0, "y1": 0, "x2": 0, "y2": 1})
+    compare(vertical, expected=[(0, 0), (0, 1)])
+
+    horizontal = line_to_points({"x1": 0, "y1": 0, "x2": 1, "y2": 0})
+    compare(horizontal, expected=[(0, 0), (1, 0)])
+
+    diagonal1 = line_to_points({"x1": 0, "y1": 0, "x2": 1, "y2": 1})
+    compare(diagonal1, expected=[(0, 0), (1, 1)])
+
+    diagonal2 = line_to_points({"x1": 0, "y1": 1, "x2": 1, "y2": 0})
+    compare(diagonal2, expected=[(0, 1), (1, 0)])
+
+
+def test_generate_simple_grid():
+    test_lines = [
+        {"x1": 0, "y1": 0, "x2": 2, "y2": 0},
+        {"x1": 0, "y1": 2, "x2": 2, "y2": 2},
+        {"x1": 0, "y1": 0, "x2": 0, "y2": 2},
     ]
-    compare(expected, actual=lines)
+    expected = [
+        [2, 1, 1],
+        [1, 0, 0],
+        [2, 1, 1],
+    ]
+    compare(expected, actual=generate_grid(test_lines))
 
 
-def test_generate_map():
-    pass
+def test_generate_grid():
+    compare(test_grid, actual=generate_grid(test_lines))
+
+
+def test_print_map():
+    expected = dedent("""\
+    .......1..
+    ..1....1..
+    ..1....1..
+    .......1..
+    .112111211
+    ..........
+    ..........
+    ..........
+    ..........
+    222111....
+    """)
+    compare(expected, actual=(print_map(test_grid)))
+
+
+def test_calculate_overlap_count():
+    compare(5, actual=calculate_overlap_count(test_grid))
+
+def test_part_two():
+    expected = dedent("""\
+        1.1....11.
+        .111...2..
+        ..2.1.111.
+        ...1.2.2..
+        .112313211
+        ...1.2....
+        ..1...1...
+        .1.....1..
+        1.......1.
+        222111....
+    """)
+    grid = generate_grid(test_lines, skip_diagonals=False)
+    compare(expected, actual=(print_map(grid)))
