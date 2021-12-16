ad841374c05f478114854cab1f2bfa5e70c60dde
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2021-12-16 23:41:18 +0000

    Day 16 Part 1

--- day16/day16_test.py
+++ day16/day16_test.py
@@ -21,16 +21,21 @@ def test__id4_packet():
     compare(packet.payload, expected=2021)
 
 def test__operator_packet():
+    compare(hex_to_binary("38006F45291200"), expected="00111000000000000110111101000101001010010001001000000000")
     packet = Packet("00111000000000000110111101000101001010010001001000000000")
     compare(packet.version, expected=1)
     compare(packet.id, expected=6)
+    compare(packet.binary, expected="0011100000000000011011110100010100101001000100100")
     compare(packet.length_type_id, expected="0")
     compare(packet.sub_packets[0].payload, expected=10)
+    compare(packet.sub_packets[0].binary_message, expected="11010001010")
     compare(packet.sub_packets[1].payload, expected=20)
+    compare(packet.sub_packets[1].binary_message, expected="0101001000100100")
 
     packet = Packet("11101110000000001101010000001100100000100011000001100000")
     compare(packet.version, expected=7)
     compare(packet.id, expected=3)
+    compare(packet.binary, expected="111011100000000011010100000011001000001000110000011")
     compare(packet.length_type_id, expected="1")
     compare(packet.sub_packets[0].payload, expected=1)
     compare(packet.sub_packets[1].payload, expected=2)
@@ -45,6 +50,10 @@ def test__operator_packet():
 
     packet = Packet(hex_to_binary("620080001611562C8802118E34"))
     compare(packet.version, expected=3)
+    compare(packet.sub_packets[0].sub_packets[0].id, expected=4)
+    compare(packet.sub_packets[0].sub_packets[1].id, expected=4)
+    compare(packet.sub_packets[1].sub_packets[0].id, expected=4)
+    compare(packet.sub_packets[1].sub_packets[1].id, expected=4)
     compare(packet.version_sum, expected=12)
 
     packet = Packet(hex_to_binary("C0015000016115A2E0802F182340"))
@@ -52,3 +61,4 @@ def test__operator_packet():
 
     packet = Packet(hex_to_binary("A0016C880162017C3686B18A3D4780"))
     compare(packet.version_sum, expected=31)
+
