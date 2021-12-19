27522be96177d410bb65be23bfe86b79ddc09071
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2021-12-19 18:55:30 +0000

    Day 19 Part 1
    + bugfix + refactor
    Can't put numpty arrays in sets, so I'm back
      to working with tuples...

--- day19/day19.py
+++ day19/day19.py
@@ -13,32 +13,31 @@ def load_day19_data(filename):
         for scanner in scanners:
             lines = scanner.split('\n')
             assert lines[0].startswith("--- scanner")
-            beacons = [[int(e) for e in line.split(',')] for line in lines[1:]]
-            beacon_arrays = [np.array(b) for b in beacons]
-            result.append(beacon_arrays)
+            beacons = [tuple((int(e) for e in line.split(','))) for line in lines[1:]]
+            result.append(beacons)
     return result
 
 
 def x_rotation_90(vector):
     """Rotates 3-D vector around x-axis"""
     R = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])
-    return np.dot(R, vector)
+    return tuple(np.dot(R, vector))
 
 
 def y_rotation_90(vector):
     """Rotates 3-D vector around y-axis"""
     R = np.array([[0, 0, 1], [0, 1, 0], [-1, 0, 0]])
-    return np.dot(R, vector)
+    return tuple(np.dot(R, vector))
 
 
 def z_rotation_90(vector):
     """Rotates 3-D vector around z-axis"""
     R = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
-    return np.dot(R, vector)
+    return tuple(np.dot(R, vector))
 
 
 def generate_permutations(beacons):
-    result = defaultdict(list)
+    result = [[] for _ in range(24)]
 
     for beacon in beacons:
         b = copy.deepcopy(beacon)
@@ -77,7 +76,7 @@ def generate_permutations(beacons):
         result[19].append(x_rotation_90(x_rotation_90(x_rotation_90(b))))
 
         # 270 around z
-        b = z_rotation_90(b)
+        b = z_rotation_90(z_rotation_90(b))
         result[20].append(b)
         result[21].append(x_rotation_90(b))
         result[22].append(x_rotation_90(x_rotation_90(b)))
@@ -89,38 +88,44 @@ def find_translation(scanner0_beacons, new_beacons):
     translation = defaultdict(int)  # for a given translation, how many beacons would get mapped?
     for beacon in scanner0_beacons:
         for maybe_the_same_beacon in new_beacons:
-            diff = beacon - maybe_the_same_beacon
-            translation[(diff[0], diff[1], diff[2])] += 1
-    for translation, count in translation.items():
-        if count >= 12:  # 12 is the minimum beacons which must register to constitute a match
-            return translation
+            diff = np.array(beacon) - np.array(maybe_the_same_beacon)
+            translation[tuple(diff)] += 1
+            if translation[tuple(diff)] == 12:
+                return tuple(diff)
+    # for translation, count in translation.items():
+    #     if count >= 12:  # 12 is the minimum beacons which must register to constitute a match
+    #         return translation
+    return None
+
+
+def attempt_translate_beacons(registered_beacons, beacons):
+    for frame_of_reference in generate_permutations(beacons):
+        translation = find_translation(registered_beacons, frame_of_reference)
+        if translation:
+            return {tuple(np.array(beacon) + np.array(translation)) for beacon in frame_of_reference}
     return None
 
 
 def register_beacons(beacons_by_scanner):
     # scanner 0 is true by definition
-    beacons = [beacon for beacon in beacons_by_scanner[0]]
-    for scanner in beacons_by_scanner[1:]:
-        all_orientations = generate_permutations(scanner)
-        for beacons_in_this_frame_of_reference in all_orientations.values():
-            translation = find_translation(beacons, beacons_in_this_frame_of_reference)
-            if translation:
-                for beacon in beacons_in_this_frame_of_reference:
-                    beacons.append(beacon + np.array(translation))
-                break
+    registered_beacons = {beacon for beacon in beacons_by_scanner[0]}
+    translated_scanners = {0}
 
-    return beacons
-# scanner #1 is the 'origin' - we will use it as a reference
+    while len(translated_scanners) < len(beacons_by_scanner):
 
-# for scanner in scanners[1:]
+        for scanner_number, beacons in enumerate(beacons_by_scanner):
+            if scanner_number in translated_scanners:
+                continue  # we've already done this scanner
 
-# this scanner can report its set of beacons with 24 different coordinates
-# depending on how it is rotated.
-# Generate a set of all of them so that we have ONE that matches the orientation of scanner 'origin'
+            translated_beacons = attempt_translate_beacons(registered_beacons, beacons)
+            if translated_beacons is not None:
+                print(f"Registration found for scanner {scanner_number}!")
+                registered_beacons.update(translated_beacons)
+                translated_scanners.add(scanner_number)
 
-# for each rotation....
-# for each beacon in the 'origin' set, generate the relative position to each scanner in the 'candidate' set
-# if we have 12 (or more) maps that are the same ... then we're a go! That rotation is a correct match
+    return registered_beacons
 
-# for each beacon in the new known set (of the correct orientation) ... shift it by the mapping
-# ... then continue with the next scanner
+if __name__ == '__main__':
+    beacons_by_scanner = load_day19_data("day19_real_data.txt")
+    beacons = register_beacons(beacons_by_scanner)
+    print(f"Once registered consistently, there are {len(beacons)} beacons present.")
--- day19/day19_test.py
+++ day19/day19_test.py
@@ -1,88 +1,82 @@
-from itertools import zip_longest
-
 from testfixtures import compare
-import numpy as np
 
 from day19.day19 import load_day19_data, generate_permutations, find_translation, register_beacons
 
 
 def test__load_day19_data():
     actual = load_day19_data("day19_test_data.txt")
-    compare(np.array_equal(actual[0][0], np.array([404, -588, -901])), expected=True)
-    compare(np.array_equal(actual[0][-1], np.array([459, -707, 401])), expected=True)
-    compare(np.array_equal(actual[-1][-1], np.array([30, -46, -14])), expected=True)
+    compare(actual[0][0], expected=(404, -588, -901))
+    compare(actual[0][-1], expected=(459, -707, 401))
+    compare(actual[-1][-1], expected=(30, -46, -14))
 
 
 def test__generate_permutations():
     beacons = [
-        np.array([1, 1, 1]),
-        np.array([2, 2, 2]),
+        (1, 1, 1),
+        (2, 2, 2),
     ]
     actual = generate_permutations(beacons)
-    expected = {
-        0: [np.array([1, 1, 1]), np.array([2, 2, 2])],
-        1: [np.array([1, -1, 1]), np.array([2, -2, 2])],
-        2: [np.array([1, -1, -1]), np.array([2, -2, -2])],
-        3: [np.array([1, 1, -1]), np.array([2, 2, -2])],
-        4: [np.array([1, 1, -1]), np.array([2, 2, -2])],
-        5: [np.array([1, 1, 1]), np.array([2, 2, 2])],  # TODO: potential bug? Need to think a bit more about the rotations...
-        6: [np.array([1, -1, 1]), np.array([2, -2, 2])],
-        7: [np.array([1, -1, -1]), np.array([2, -2, -2])],
-        8: [np.array([-1, 1, -1]), np.array([-2, 2, -2])],
-        9: [np.array([-1, 1, 1]), np.array([-2, 2, 2])],
-        10: [np.array([-1, -1, 1]), np.array([-2, -2, 2])],
-        11: [np.array([-1, -1, -1]), np.array([-2, -2, -2])],
-        12: [np.array([-1, 1, 1]), np.array([-2, 2, 2])],
-        13: [np.array([-1, -1, 1]), np.array([-2, -2, 2])],
-        14: [np.array([-1, -1, -1]), np.array([-2, -2, -2])],
-        15: [np.array([-1, 1, -1]), np.array([-2, 2, -2])],
-        16: [np.array([-1, 1, 1]), np.array([-2, 2, 2])],
-        17: [np.array([-1, -1, 1]), np.array([-2, -2, 2])],
-        18: [np.array([-1, -1, -1]), np.array([-2, -2, -2])],
-        19: [np.array([-1, 1, -1]), np.array([-2, 2, -2])],
-        20: [np.array([-1, -1, 1]), np.array([-2, -2, 2])],
-        21: [np.array([-1, -1, -1]), np.array([-2, -2, -2])],
-        22: [np.array([-1, 1, -1]), np.array([-2, 2, -2])],
-        23: [np.array([-1, 1, 1]), np.array([-2, 2, 2])],
-    }
-    compare(set(actual.keys()), expected=set(expected.keys()))
-    for key in actual.keys():
-        for e, a in zip_longest(expected[key], actual[key]):
-            compare(np.array_equal(e, a), expected=True)
+    expected = [
+        [(1, 1, 1), (2, 2, 2)],
+        [(1, -1, 1), (2, -2, 2)],
+        [(1, -1, -1), (2, -2, -2)],
+        [(1, 1, -1), (2, 2, -2)],
+        [(1, 1, -1), (2, 2, -2)],
+        [(1, 1, 1), (2, 2, 2)],
+        [(1, -1, 1), (2, -2, 2)],
+        [(1, -1, -1), (2, -2, -2)],
+        [(-1, 1, -1), (-2, 2, -2)],
+        [(-1, 1, 1), (-2, 2, 2)],
+        [(-1, -1, 1), (-2, -2, 2)],
+        [(-1, -1, -1), (-2, -2, -2)],
+        [(-1, 1, 1), (-2, 2, 2)],
+        [(-1, -1, 1), (-2, -2, 2)],
+        [(-1, -1, -1), (-2, -2, -2)],
+        [(-1, 1, -1), (-2, 2, -2)],
+        [(-1, 1, 1), (-2, 2, 2)],
+        [(-1, -1, 1), (-2, -2, 2)],
+        [(-1, -1, -1), (-2, -2, -2)],
+        [(-1, 1, -1), (-2, 2, -2)],
+        [(1, -1, 1), (2, -2, 2)],
+        [(1, -1, -1), (2, -2, -2)],
+        [(1, 1, -1), (2, 2, -2)],
+        [(1, 1, 1), (2, 2, 2)],
+    ]
+    compare(actual, expected=expected)
 
 
 def test_find_translation():
     beacons_seen_by_scanner_0 = [
-        np.array([-618, -824, -621]),
-        np.array([-537, -823, -458]),
-        np.array([-447, -329, 318]),
-        np.array([404, -588, -901]),
-        np.array([544, -627, -890]),
-        np.array([528, -643, 409]),
-        np.array([-661, -816, -575]),
-        np.array([390, -675, -793]),
-        np.array([423, -701, 434]),
-        np.array([-345, -311, 381]),
-        np.array([459, -707, 401]),
-        np.array([-485, -357, 347]),
+        (-618, -824, -621),
+        (-537, -823, -458),
+        (-447, -329, 318),
+        (404, -588, -901),
+        (544, -627, -890),
+        (528, -643, 409),
+        (-661, -816, -575),
+        (390, -675, -793),
+        (423, -701, 434),
+        (-345, -311, 381),
+        (459, -707, 401),
+        (-485, -357, 347),
     ]
     beacons_seen_by_scanner_1 = [
-        np.array([686, 422, 578]),
-        np.array([605, 423, 415]),
-        np.array([515, 917, -361]),
-        np.array([-336, 658, 858]),
-        np.array([-476, 619, 847]),
-        np.array([-460, 603, -452]),
-        np.array([729, 430, 532]),
-        np.array([-322, 571, 750]),
-        np.array([-355, 545, -477]),
-        np.array([413, 935, -424]),
-        np.array([-391, 539, -444]),
-        np.array([553, 889, -390]),
+        (686, 422, 578),
+        (605, 423, 415),
+        (515, 917, -361),
+        (-336, 658, 858),
+        (-476, 619, 847),
+        (-460, 603, -452),
+        (729, 430, 532),
+        (-322, 571, 750),
+        (-355, 545, -477),
+        (413, 935, -424),
+        (-391, 539, -444),
+        (553, 889, -390),
     ]
     found_translation = None
     permutations = generate_permutations(beacons_seen_by_scanner_1)
-    for k, beacons_in_this_frame_of_reference in permutations.items():
+    for beacons_in_this_frame_of_reference in permutations:
         translation = find_translation(beacons_seen_by_scanner_0, beacons_in_this_frame_of_reference)
         if translation:
             found_translation = translation
