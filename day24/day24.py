093cc6a4e82cac985f67cade7811277c76bce861
Alex Zawadzki <alexander.zawadzki@gmail.com>
2021-12-24 22:49:35 +0000

    Day 24 Part 0.6
    Less OO attempt to brute force it with faster exec()  - still looks too slow :(

--- day24/day24.py
+++ day24/day24.py
@@ -2,91 +2,52 @@ import os.path
 from collections import deque
 
 
-class Alu(object):
-
-    def __init__(self, input_string, steps) -> None:
-        super().__init__()
-        self.input_buffer = deque([int(x) for x in input_string])
-        self.steps = steps
-        self.x = 0
-        self.y = 0
-        self.z = 0
-        self.w = 0
-
-    def input(self, target):
-        self.__setattr__(target, self.input_buffer.popleft())
-
-    def add(self, a, b):
-        if not isinstance(b, int):
-            b = self.__getattribute__(b)
-        self.__setattr__(a, self.__getattribute__(a) + b)
-
-    def mul(self, a, b):
-        if not isinstance(b, int):
-            b = self.__getattribute__(b)
-        self.__setattr__(a, self.__getattribute__(a) * b)
-
-    def div(self, a, b):
-        if not isinstance(b, int):
-            b = self.__getattribute__(b)
-        self.__setattr__(a, self.__getattribute__(a) // b)
-
-    def mod(self, a, b):
-        if not isinstance(b, int):
-            b = self.__getattribute__(b)
-        self.__setattr__(a, self.__getattribute__(a) % b)
-
-    def eql(self, a, b):
-        if not isinstance(b, int):
-            b = self.__getattribute__(b)
-        self.__setattr__(a, 1 if self.__getattribute__(a) == b else 0)
-
-    def do_steps(self):
-        for (instruction, a, b) in self.steps:
-            if instruction == 'inp':
-                self.input(a)
-            elif instruction == 'add':
-                self.add(a,b)
-            elif instruction == 'mul':
-                self.mul(a, b)
-            elif instruction == 'div':
-                self.div(a, b)
-            elif instruction == 'mod':
-                self.mod(a, b)
-            elif instruction == 'eql':
-                self.eql(a, b)
-            else:
-                raise ValueError(f'Unexpected instruction {instruction}')
-
-
-def text2steps(text):
-    result = []
-    for line in text.split('\n'):
-        thisline = line.split(' ')
-        if len(thisline) == 3 and thisline[2] not in ('x', 'y', 'z', 'w'):
-            thisline[2] = int(thisline[2])
-        if len(thisline) ==2:
-            thisline.append(None)
-        result.append(thisline)
-    return result
-
-
 def load_day_24_data(filename):
     filepath = os.path.join(os.path.dirname(__file__), filename)
     with open(filepath) as f:
-        text = f.read()
-    return text2steps(text)
+        text = f.readlines()
+    op2operator = {
+        "add": '+=',
+        "mul": '*=',
+        "div": '//=',
+        "mod": '%=',
+    }
+    result = ["global w, x, y, z", "w, x, y, z,  = 0, 0, 0, 0"]
+    for line in text:
+        if line.startswith("inp"):
+            result.append("w = candidate.popleft()\n")
+        elif line[0:3] in op2operator:
+            result.append(f"{line[4]} {op2operator[line[0:3]]} {line[6:]}")
+        else:
+            result.append(f"{line[4]} = 1 if {line[4]} == {line[6:-1].strip()} else 0")
+    return '\n'.join([r.strip() for r in result])
+
+def compute(candidate):
+    str_candidate = str(candidate)
+    if '0' in str_candidate:
+        return None, None, None, None
+    # print(str_candidate)
+    candidate = deque([int(c) for c in str_candidate])
+    exec(steps)
+    # print(f"now w={w}, x={x}, y={y}, z={z}")
+    return w, x, y, z
+
+
 
 if __name__ == '__main__':
     steps = load_day_24_data("day24_real_data.txt")
     for potential_model_number in range(99999999999999, 0, -1):
-        str_model_no = str(potential_model_number)
-        if '0' in str_model_no:
-            continue
-        else:
-            # print(f'trying {str_model_no}')
-            alu = Alu(str_model_no, steps)
-            alu.do_steps()
-            if alu.z == 0:
-                print(f"The submarine model is {str_model_no}")
-                break
+        w, x, y, z = compute(potential_model_number)
+        if z == 0:
+            print(f"****** {potential_model_number} ********")
+            break
+    #     str_model_no = str(potential_model_number)
+    #     if '0' in str_model_no:
+    #         continue
+    #     else:
+    #         # print(f'trying {str_model_no}')
+    #         alu = Alu(str_model_no, steps)
+    #         alu.do_steps()
+    #         if alu.z == 0:
+    #             print(f"The submarine model is {str_model_no}")
+    #             break
